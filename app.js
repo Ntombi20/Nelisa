fb1c2eb3ff1f7ed6d69faa33865688e25e69d5f0
Ntombi20 <ntombi@projectcodex.co>
2016-06-29 15:55:31 +0200

    displayed the first week

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
+var handlebars = require('./handlebars-v4.0.5');
 var mostLeastProduct = require('./mostLeastProduct');
+var groupProduct = require('./groupProduct');
+var category = require('./category');
+var groupCategory = require('./groupCategory');
+var profit = require('./profit');
+var groupPurchases = require('./groupPurchases');
 
+//procssing the data to get the reports for productsSold
+var week1 = groupProduct.readRecords('./files/week1.csv');
+var groupWeek1 = groupProduct.groupRecords(week1);
+var mostProduct1 = mostLeastProduct.mostProduct(groupWeek1);
 
+var source = fs.readFileSync('./index2.hbs', "utf8");
 
-var mostProduct1 = mostLeastProduct.mostProduct("expectedWeek1");
-console.log(mostProduct1);
+var template = handlebars.compile(source);
 
-// var source = fs.readFileSync(productsSold, "utf8");
-//console.log(productsSold);
-// var mostProduct1 = mostLeastProduct.mostProduct(expectedWeek1);
+var data = {key: [mostProduct1]}
+
+fs.writeFileSync('index.html', template(data))
