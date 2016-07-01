b75b9f48fb65bde58c257929c4e90e5f0e174c23
Ntombi20 <ntombi@projectcodex.co>
2016-07-01 10:21:08 +0200

    fix the category test nd function

--- category.js
+++ category.js
@@ -1,3 +1,32 @@
+//Should group the data into categorys
+exports.groupCategory = function(category, productMap) {
+
+        var fs = require('fs');
+        var categoryMap = {};
+        var readFile = fs.readFileSync(category, "utf8");
+        var group = readFile.split('\n').slice(1).filter(Boolean);
+
+        group.forEach(function(groupCategory) {
+            var item = groupCategory.split(",");
+            var categoryName = item[0];
+            var categoryItem = item[1];
+
+            if (categoryMap[categoryItem] === undefined) {
+                categoryMap[categoryItem] = "";
+            }
+            categoryMap[categoryItem] = categoryMap[categoryItem] + categoryName;
+        })
+
+        var categoryGrouped = {};
+
+        for (var product in productMap) {
+            if (categoryGrouped[categoryMap[product]] === undefined) {
+                categoryGrouped[categoryMap[product]] = 0;
+            }
+            categoryGrouped[categoryMap[product]] = categoryGrouped[categoryMap[product]] + productMap[product];
+        }
+        return categoryGrouped;
+};
 //the most popular category sold each week.
 exports.mostCategory = function(categoryMap) {
     var qty = 0;
