dcf5facac3a640e080a259184098b464b3bf0b60
Ntombi20 <ntombi@projectcodex.co>
2016-07-07 14:53:25 +0200

    handlebars

---
+++ app1.js
@@ -0,0 +1,15 @@
+var express = require('./express-app/node_modules/express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3010, function(){
+  console.log("listening on port 3010");
+});
