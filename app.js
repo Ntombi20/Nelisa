0682396fa076e7a73952100c29c454a4c2935f41
Ntombi20 <ntombi@projectcodex.co>
2016-07-09 16:32:53 +0200

    display sales

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var fs = require('fs');
-var handlebars = require('./handlebars-v4.0.5');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 //procssing the data to get the reports
 
 function weelkySalesStats(week) {
@@ -12,7 +17,7 @@ function weelkySalesStats(week) {
   var mostProduct = mostLeastProduct.mostProduct(groupWeek);
   var leastProduct = mostLeastProduct.leastProduct(groupWeek);
   var weekCategory = category.groupCategory("./files/category.csv", groupWeek);
-  var popularSalesCategory = category.mostCategory(weekCategory);
+  var popularSalesProduct = category.mostCategory(weekCategory);
   var unpopularSalesCategory = category.leastCategory(weekCategory);
   var weekdate = new Date("7-Feb");
   var weekdate = new Date("14-Feb");
@@ -28,7 +33,7 @@ function weelkySalesStats(week) {
   var sales = {
     mostProduct: mostProduct,
     leastProduct: leastProduct,
-    popularSalesCategory: popularSalesCategory,
+    popularSalesProduct: popularSalesProduct,
     unpopularSalesCategory: unpopularSalesCategory,
     profitableProduct: profitableProduct,
     profitableCategory: profitableCategory
@@ -37,30 +42,31 @@ function weelkySalesStats(week) {
   return sales;
 }
 
-var data1 = weelkySalesStats('./files/week1.csv');
-var data2 = weelkySalesStats('./files/week2.csv');
-var data3 = weelkySalesStats('./files/week3.csv');
-var data4 = weelkySalesStats('./files/week4.csv');
+var week1 = weelkySalesStats('./files/week1.csv');
+var week2 = weelkySalesStats('./files/week2.csv');
+var week3 = weelkySalesStats('./files/week3.csv');
+var week4 = weelkySalesStats('./files/week4.csv');
 
-var source = fs.readFileSync('./views/index.handlebars', "utf8");
-
-var template = handlebars.compile(source);
-
-fs.writeFileSync('week1.html', template(data1))
-fs.writeFileSync('week2.html', template(data2))
-fs.writeFileSync('week3.html', template(data3))
-fs.writeFileSync('week4.html', template(data4))
-
-var express = require('./express-app/node_modules/express');
-var exphbs  = require('express-handlebars');
-
-var app = express();
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/sales/week1', function (req, res) {
+    res.render('week1', {
+      week: week1
+    });
+});
+app.get('/sales/week2', function (req, res) {
+    res.render('week2', {
+      week: week2
+    });
+});
+app.get('/sales/week3', function (req, res) {
+    res.render('week3', {
+      week: week3
+    });
+});
+app.get('/sales/week4', function (req, res) {
+    res.render('week4', {
+      week: week4
+    });
 });
 
-app.listen(3000);
+app.listen(3010);
+console.log("running port 3010")
