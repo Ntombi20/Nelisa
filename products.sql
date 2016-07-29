80d110bca86fc562c8863a0cc4abbc0370a857a6
ntombi20 <ntombi@projectcodex.co>
2016-07-29 11:47:24 +0200

    products table

---
+++ products.sql
@@ -0,0 +1,9 @@
+drop table if exists 'products';
+
+create table products (
+    id int primary key auto_increment,
+        description char(100) not null,
+    price decimal(10,2),
+    category_id int,
+    foreign key (category_id) references categories(id)
+);
