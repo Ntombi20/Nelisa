f1f849a9116507d62bdf912a76b8aaf78df26f0b
ntombi20 <ntombi@projectcodex.co>
2016-08-15 10:02:09 +0200

    done

--- SQL_tables/suppliers.sql
+++ SQL_tables/suppliers.sql
@@ -2,5 +2,6 @@ drop table if exists suppliers;
 
 create table suppliers (
   id int primary key auto_increment,
-  shop char(100) not null
+  shop char(100) not null,
+  CONSTRAINT uc_id UNIQUE (id)
 );
