9a99b38487529110affc870c619ba262b9874e08
ntombi20 <ntombi@projectcodex.co>
2016-08-15 09:52:38 +0200

    update

--- routes/sales.js
+++ routes/sales.js
@@ -9,3 +9,79 @@ exports.show = function (req, res, next) {
       	});
 	});
 };
+
+exports.showAdd = function(req, res){
+	req.getConnection(function(err, connection){
+		if (err) return next(err);
+		connection.query('SELECT * from products', [], function(err, products) {
+        	if (err) return next(err);
+    		res.render( 'add_sales', {
+					products : products,
+    		});
+      	});
+	});
+};
+
+exports.add = function (req, res, next) {
+	req.getConnection(function(err, connection){
+		if (err) return next(err);
+		var data = {
+			products_id : Number(req.body.products_id),
+      date : req.body.date,
+			quantity : req.body.quantity,
+			price : req.body.price,
+  		};
+
+		connection.query('insert into sales set ?', data, function(err, results) {
+  			if (err) return next(err);
+				res.redirect('/sales');
+		});
+	});
+};
+
+// exports.get = function(req, res, next){
+// 	var id = req.params.id;
+// 	req.getConnection(function(err, connection){
+// 		connection.query('SELECT * FROM categories', [id], function(err, categories){
+// 			if(err) return next(err);
+// 			connection.query('SELECT * FROM products WHERE id = ?', [id], function(err,products){
+// 				if(err) return next(err);
+// 				var product = products[0];
+// 				categories = categories.map(function(category){
+// 					category.selected = category.id === product.category_id ? "selected" : "";
+// 					return category;
+// 				});
+// 				res.render('edit_product', {
+// 					categories : categories,
+// 					data : product
+// 				});
+// 			});
+// 		});
+// 	});
+// };
+//
+// exports.update = function(req, res, next){
+//
+// 	var data = {
+// 		category_id : Number(req.body.category_id),
+// 		product : req.body.product,
+// 	};
+//   	var id = req.params.id;
+//   	req.getConnection(function(err, connection){
+// 		if (err) return next(err);
+// 		connection.query('UPDATE products SET ? WHERE id = ?', [data, id], function(err, rows){
+// 			if (err) return next(err);
+//       		res.redirect('/products');
+// 		});
+//     });
+// };
+//
+// exports.delete = function(req, res, next){
+// 	var id = req.params.id;
+// 	req.getConnection(function(err, connection){
+// 		connection.query('DELETE FROM products WHERE id = ?', [id], function(err,rows){
+// 			if(err) return next(err);
+// 			res.redirect('/products');
+// 		});
+// 	});
+// };
