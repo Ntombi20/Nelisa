7912ac05c63b60e570e66aa6e0df07b4b827c521
Ntombi Masango <ntombi@projectcodex.co>
2016-11-02 15:01:51 +0200

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -4,38 +4,29 @@ exports.login = function(req, res, next) {
     req.getConnection(function(err, connection) {
         var data = {
             username: req.body.username,
-            password: req.body.password,
+            password: req.body.password
         };
+        
         connection.query('select * from users where username ?', data.username, function(err, results) {
-          console.log('login...' + err);
           
           if (err) return next(err);
-
+            var user = results[0];
 
             //check if a user exist
-            // if (data.username === undefined) {
-            //     console.log("User does not exist");
-            //     return res.redirect("/login");
-            // }
-
-
-                brcypt.compare(data.password, users.password, function(err, pass) {
-                  console.log(pass);
-                  if (err) {
-                      console.log("Error inserting : %s ", err);
-                      return res.redirect("/error?error=" + err)
-                  };
-
-                  res.redirect('/login');
-                    // if (pass) {
-                    //     req.session.user = username;
-                    //     return res.redirect('/home');
-                    // } else {
-                    //     return res.redirect("/login");
-                    //     console.log(err);
-                    // };
+            if (user === undefined) {
+                console.log("User does not exist");
+                return res.redirect("/login");
+            }
+            else{
+                brcypt.compare(data.password, user.password, function(err, pass) {
+                       if (pass) {
+                           req.session.user = username;
+                            return res.redirect('/home');
+                        } else {
+                            return res.redirect("/");
+                        };
                 });
-            // };
+             };
         });
 
     });
