3f8d5dd5dd3d9ad372fa6a305bde7e5f37a2acd5
Ntombi Masango <ntombi@projectcodex.co>
2016-11-02 15:03:10 +0200

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -6,27 +6,26 @@ exports.login = function(req, res, next) {
             username: req.body.username,
             password: req.body.password
         };
-        
+
         connection.query('select * from users where username ?', data.username, function(err, results) {
-          
-          if (err) return next(err);
+
+            if (err) return next(err);
             var user = results[0];
 
             //check if a user exist
             if (user === undefined) {
                 console.log("User does not exist");
                 return res.redirect("/login");
-            }
-            else{
+            } else {
                 brcypt.compare(data.password, user.password, function(err, pass) {
-                       if (pass) {
-                           req.session.user = username;
-                            return res.redirect('/home');
-                        } else {
-                            return res.redirect("/");
-                        };
+                    if (pass) {
+                        req.session.user = username;
+                        return res.redirect('/home');
+                    } else {
+                        return res.redirect("/");
+                    };
                 });
-             };
+            };
         });
 
     });
